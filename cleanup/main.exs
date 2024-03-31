5baf8b27e083d8b3f5b9ec8be3719a57d692f1d3
AStaniszewski <staniszewski01adam@gmail.com>
2024-03-31 19:58:25 +0200

    31.03: XML to JSON logic implemented, still need to work on correct parsing

--- cleanup/main.exs
+++ cleanup/main.exs
@@ -1,9 +1,57 @@
 defmodule DatasetParser do
+  Mix.install([
+    {:jason, "~> 1.4"},
+    {:poison, "~> 5.0"},
+    {:elixir_xml_to_map, "~> 2.0"}
+  ])
+
   def read_xml(filepath) do
-    IO.puts(filepath)
+    case File.read(filepath) do
+      {:ok, content} -> {:ok, content}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def xml_to_json({:ok, content}) do
+    case XmlToMap.naive_map(content) do
+      {:ok, xml_map} -> {:ok, Jason.encode!(xml_map)}
+      {:error, reason} -> {:error, reason}
+    end
+
+  end
+
+  def xml_to_json({:error, reason}) do
+    {:error, reason}
+  end
+
+  def save_json(filepath, json_content) do
+    case File.write(filepath, json_content) do
+      :ok -> {:ok, "JSON content saved to #{filepath}"}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def print_xml({:ok, content}) do
+    IO.puts(content)
+  end
+
+  def print_xml({:error, reason}) do
+    Io.puts("Error while reading XML file: #{reason}")
   end
 end
 
 filepath = "E:\\Users\\PanSt\\OneDrive\\Pulpit\\Diorisis\\Diorisis\\Achilles Tatius (0532) - Leucippe and Clitophon (001).xml"
+output_filepath = "E:\\Users\\PanSt\\OneDrive\\Pulpit\\Eliksir\\cleanup\\filedump"
 
-DatasetParser.read_xml(filepath)
+case DatasetParser.read_xml(filepath) do
+  {:ok, xml_content} ->
+    case DatasetParser.xml_to_json({:ok, xml_content}) do
+      {:ok, json_content} ->
+        case DatasetParser.save_json(output_filepath, json_content) do
+          {:ok, _} -> IO.puts("JSON content saved.")
+          {:error, reason} -> IO.puts("Failes to save content")
+        end
+        {:error, reason} -> IO.puts("Failed to convert XML to JSON")
+    end
+    {:error, reason} -> IO.puts("Failed to read XML file")
+end
--- cleanup/mix.exs
+++ cleanup/mix.exs
@@ -14,7 +14,7 @@ defmodule Cleanup.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :elixir_xml_to_map]
+      extra_applications: [:logger, :jason, :elixir_xml_to_map]
     ]
   end
 
@@ -24,6 +24,7 @@ defmodule Cleanup.MixProject do
       {:jason, "~> 1.4"},
       {:poison, "~> 5.0"},
       {:elixir_xml_to_map, "~> 2.0"}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
