5a67853d8866e61e545448119199c89db10438af
AStaniszewski <staniszewski01adam@gmail.com>
2024-03-31 18:24:38 +0200

    31.03

--- cleanup/main.exs
+++ cleanup/main.exs
@@ -1,21 +1,9 @@
 defmodule DatasetParser do
-  def read_json(filepath) do
-    case File.read(filepath) do
-      {:ok, content} -> {:ok, content}
-      {:error, reason} -> {:error, reason}
-    end
-  end
-
-  def print_json({:ok, content}) do
-    IO.puts("JSON content: #{content}")
-  end
-
-  def print_json({:error, reason}) do
-    IO.puts("Error reading file: #{reason}")
+  def read_xml(filepath) do
+    IO.puts(filepath)
   end
 end
 
 filepath = "E:\\Users\\PanSt\\OneDrive\\Pulpit\\Diorisis\\Diorisis\\Achilles Tatius (0532) - Leucippe and Clitophon (001).xml"
 
-DatasetParser.read_json(filepath)
-|> DatasetParser.print_json()
+DatasetParser.read_xml(filepath)
