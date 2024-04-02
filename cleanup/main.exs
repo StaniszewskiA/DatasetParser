02b69031c6d530fcb947c815df354d68531a8bda
AStaniszewski <staniszewski01adam@gmail.com>
2024-04-02 20:16:37 +0200

    02.04

--- cleanup/main.exs
+++ cleanup/main.exs
@@ -1,27 +1,11 @@
 defmodule DatasetParser do
-  Mix.install([
-    {:jason, "~> 1.4"},
-    {:poison, "~> 5.0"},
-    {:elixir_xml_to_map, "~> 2.0"},
-    {:sweet_xml, "~> 0.7.4"}
-  ])
-  @derive [Poison.Encoder]
-  @derive SweetXml
-
-  def read_file(filepath)do
-    case File.read(filepath) do
-      {:ok, file_content} ->
-        case SweetXml.parse(file_content) do
-          {:ok, xml} ->
-            json = Poison.encode!(xml)
-            IO.puts(filepath)
-          {:error, _reason} -> IO.puts("Unable to parse file content")
-        end
-      {:error, reason} -> IO.puts(reason)
-    end
+  def read_file(filepath) do
+    IO.puts(filepath)
   end
 end
 
-filepath = "E:\\Users\\PanSt\\OneDrive\\Pulpit\\Diorisis\\Diorisis\\Achilles Tatius (0532) - Leucippe and Clitophon (001).xml"
+# dupa
+
+filepath = "E:\\Users\\PanSt\\OneDrive\\Pulpit\\Diorisis\\DiorisisCorpus1.51\\Achilles Tatius (0532) - Leucippe and Clitophon (001).json"
 
 DatasetParser.read_file(filepath)
