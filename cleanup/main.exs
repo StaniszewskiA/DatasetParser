00088a598cfcf42f741e6e470fa9ad5b278f3df2
AStaniszewski <staniszewski01adam@gmail.com>
2024-04-04 13:40:51 +0200

    04.04: Counting jsons in the dir

--- cleanup/main.exs
+++ cleanup/main.exs
@@ -1,12 +1,14 @@
 defmodule DatasetParser do
-  def traverse_directory(folder_path) do
+  def count_jsons(folder_path) do
     folder_path
     |> Path.expand()
     |> File.ls!()
-    |> Enum.each(&IO.puts(&1))
+    |> Enum.filter(&Path.extname(&1) == ".json")
+    |> length()
+    |> IO.puts()
   end
 end
 
 folder_path = "E:\\Users\\PanSt\\OneDrive\\Pulpit\\Diorisis\\DiorisisCorpus1.51"
 
-DatasetParser.traverse_directory(folder_path)
+DatasetParser.count_jsons(folder_path)
