dfeabf16038aea1f6d5c71b3f3981962bbb88d08
AStaniszewski <staniszewski01adam@gmail.com>
2024-04-05 17:34:44 +0200

    05.04: Choosing a JSON file

--- cleanup/main.exs
+++ cleanup/main.exs
@@ -7,8 +7,22 @@ defmodule DatasetParser do
     |> length()
     |> IO.puts()
   end
+  def list_jsons(folder_path) do
+    folder_path
+    |> Path.expand()
+    |> File.ls!()
+    |> Enum.each(&IO.puts(&1))
+  end
+  def choose_json(folder_path, index) do
+    folder_path
+    |> Path.expand()
+    |> File.ls!()
+    |> Enum.filter(&Path.extname(&1) == ".json")
+    |> Enum.at(index - 1)
+  end
 end
 
 folder_path = "E:\\Users\\PanSt\\OneDrive\\Pulpit\\Diorisis\\DiorisisCorpus1.51"
+index = 1
 
-DatasetParser.count_jsons(folder_path)
+IO.puts(DatasetParser.choose_json(folder_path, index))
